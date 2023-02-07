9573bf4f951fa042209cddc3fc4c95054802ea3f
vedant-11 <v.vedantsingh.11@gmail.com>
2023-02-08 02:48:36 +0530

    added the bot commands

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+import invariant from "tiny-invariant";
+import { Configuration, OpenAIApi } from "openai";
+import { brotliCompressSync } from "zlib";
+
+config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const openai = new OpenAIApi(configuration);
+
+invariant(token, "Couldn't read the token the enviroment variable");
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${msg.text}.\n`,
+    temperature: 0.7,
+    max_tokens: 800,
+  });
+
+  const chatId = msg.chat.id;
+
+  //   bot.sendMessage(chatId, "");
+
+  const basePromptOuput = baseCompletion.data.choices.pop();
+
+  if (!basePromptOuput?.text) {
+    return bot.sendMessage(
+      chatId,
+      "please try again, AI couldn't send the data"
+    );
+  }
+
+  bot.sendMessage(chatId, basePromptOuput?.text);
+});
+
+bot.on("error", (err) => console.log(err));
